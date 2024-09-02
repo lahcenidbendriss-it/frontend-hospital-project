a301e898694968c1c4fd40b5246d2ea288b87d10
zux <zux@zux-MacBook-Pro.local>
2024-09-02 21:54:34 +0100

    update info show all them

--- src/Info.js
+++ src/Info.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import Footer from './Footer';
 export function handleDownload (userInfo){
     const content = `
-    PPR: ${userInfo.PPR}
+    PPR: ${userInfo.ppr}
     Nom: ${userInfo.Nom}
     Prenom: ${userInfo.Prenom}
     Grade: ${userInfo.Grade}
@@ -17,7 +17,7 @@ const blob = new Blob([content], { type: 'text/plain' });
 const url = URL.createObjectURL(blob);
 const link = document.createElement('a');
 link.href = url;
-link.setAttribute('download', `${userInfo.PPR}_info.txt`);
+link.setAttribute('download', `${userInfo.ppr}_info.txt`);
 document.body.appendChild(link);
 link.click();
 document.body.removeChild(link);
@@ -42,7 +42,7 @@ function Info() {
             axios.delete(`https://backend-hospital-project.onrender.com/delete/${ppr}`)
                 .then(res => {
                     console.log(res.data);
-                    setEmployees(employees.filter(employee => employee.PPR !== ppr));
+                    setEmployees(employees.filter(employee => employee.ppr !== ppr));
                 })
                 .catch(err => console.log(err));
         }
@@ -69,18 +69,18 @@ function Info() {
                 </thead>
                 <tbody>
                     {employees.map(employee =>
-                        <tr key={employee.PPR}>
-                        <td>{employee.PPR}</td>
-                            <td>{employee.Nom}</td>
-                            <td>{employee.Prenom}</td>
-                            <td>{employee.Grade}</td>
-                            <td>{employee.Type_conge}</td>
-                            <td>{employee.Date_de_conge}</td>
-                            <td>{employee.Statu}</td>
+                        <tr key={employee.ppr}>
+                        <td>{employee.ppr}</td>
+                            <td>{employee.nom}</td>
+                            <td>{employee.prenom}</td>
+                            <td>{employee.grade}</td>
+                            <td>{employee.type_conge}</td>
+                            <td>{employee.date_de_conge}</td>
+                            <td>{employee.statu}</td>
                             <td>{employee.service}</td>
                             <td>
-                                <button onClick={() => handleDelete(employee.PPR)} className='btn btn-danger m-1'><i className="fa fa-trash"></i> Delete</button>
-                                <Link to={`/update/${employee.PPR}`} className='btn btn-info '><i className="fa fa-edit"></i> Update</Link>
+                                <button onClick={() => handleDelete(employee.ppr)} className='btn btn-danger m-1'><i className="fa fa-trash"></i> Delete</button>
+                                <Link to={`/update/${employee.ppr}`} className='btn btn-info '><i className="fa fa-edit"></i> Update</Link>
                             </td>
                             <td>
                             <button onClick={() => handleDownload(employee)} className='btn btn-primary'><i className="fa fa-download"></i> Download</button>
